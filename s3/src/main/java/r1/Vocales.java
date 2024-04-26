01c9d9f967c351625add82b50d8217a6a4c11742
Andrés Ramírez <andres200493@gmail.com>
2024-04-25 19:54:01 -0600

    S3 -> R1

---
+++ s3/src/main/java/r1/Vocales.java
@@ -0,0 +1,23 @@
+package r1;
+
+import java.util.Scanner;
+
+public class Vocales {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Escribe la palabra que se analizará: ");
+
+        String palabra = scanner.nextLine();
+
+        int conteoVocales = 0;
+
+        for (char caracter : palabra.toLowerCase().toCharArray()) {
+            if (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u') {
+                conteoVocales++;
+            }
+        }
+
+        System.out.println(palabra + ": " + conteoVocales);
+    }
+}
