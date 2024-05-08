afa2f851c514c7427e63169f715bd30cc3e97bd2
Andrés Ramírez <andres200493@gmail.com>
2024-05-07 19:40:52 -0600

    S6 -> E1

--- s6/src/main/java/e1/Polimorfismo.java
+++ s6/src/main/java/e1/Polimorfismo.java
@@ -1,9 +1,17 @@
 package e1;
 
+import java.util.Scanner;
+
 public class Polimorfismo {
     public static void main(String[] args) {
         Venado venado = new Venado(24);
         venado.camina();
         venado.comerPlantas();
+
+        System.out.println("Venado: " + (venado instanceof Venado));
+        System.out.println("Animal: " + (venado instanceof Animal));
+        System.out.println("Hervivoro: " + (venado instanceof Hervivoro));
+        System.out.println("Object: " + (venado instanceof Object));
+//        System.out.println("Object: " + (venado instanceof Scanner)); // False
     }
 }
